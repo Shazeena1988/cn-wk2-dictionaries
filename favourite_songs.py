8554843547a49333c1ae229304a905fc4190b020
Shazeena Hussain <shazeena1988@gmail.com>
2024-02-06 15:35:38 +0000

    Add favourite songs to dictionary

---
+++ favourite_songs.py
@@ -0,0 +1,9 @@
+# Ceating a dictionary of my favourite songs
+
+favourite_songs = {
+    "Tracy Chapman" : "Fast car",
+    "Khalid" : "OTW",
+    "Jaheim" : "Anything",
+    "Whitney Houston" : "It's not right but it's ok",
+    "Taylor Swift" : "False god",
+}
